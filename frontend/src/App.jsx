c96995bba2acb609dd84076d052a743a18df6727
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-06-27 06:48:42 +0530

    Divide frontend into pages and components also use some module for routing BrowserRoutes, Routes, Route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className='text-3xl'>
-      This is knowledgeBase
+    <div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/> 
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
