b743a7a53bb4456604837d3d30c6ae6a4d03486b
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-07-01 10:55:53 +0530

    create test api with  route and controller

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/user', userRoutes);
+
+
+connectDB()
+.then(
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port ${process.env.PORT || 3000}`);
+    })
+)
+.catch((error) => {
+    console.log(`Mongodb connection failed: ${error}`);
+});
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>}/> 
-        </Routes>
+        <Header>
+          <Routes>
+              <Route path="/" element={<Home/>}/> 
+          </Routes>
+        </Header>
       </BrowserRouter>
     </div>
   )
