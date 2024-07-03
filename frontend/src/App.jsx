c5b7cd3a07defa0c96300fc5b5f2b036ee451f1b
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-07-04 04:43:26 +0530

    Degrade "flowbite-react": "^0.10.1", to "^0.7.0"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Header>
-          <Routes>
-              <Route path="/" element={<Home/>}/> 
-          </Routes>
-        </Header>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+       
+        
+      </Routes>
+        
+    </BrowserRouter>
   )
 }
