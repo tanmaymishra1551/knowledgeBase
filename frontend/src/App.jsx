f40563717478ffd460a77ae354b84909540a7636
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-07-09 05:50:34 +0530

    complete sidebar of the dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import Home from './pages/Home.jsx'
 import SignUp from './pages/Signup.jsx'
 import Header from './components/Header.jsx'
 import Signin from './pages/Signin.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 import FooterCom from './components/Footer.jsx'
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -13,6 +15,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/sign-in' element={<Signin />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         
       </Routes>
       <FooterCom />
