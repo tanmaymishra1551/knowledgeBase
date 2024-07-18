6f9b4611ed3e1cbd34ff6bc9cb757188b2d977d1
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-07-18 17:44:08 +0530

    complete user delete functionality at frontend side

--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -36,11 +36,28 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
   },
 });
 
 export const { signInStart, signInSuccess, signInFailure, updateStart,
   updateSuccess,
-  updateFailure,} = userSlice.actions;
+  updateFailure, deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,} = userSlice.actions;
 
 export default userSlice.reducer;
