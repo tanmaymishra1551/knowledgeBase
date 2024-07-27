658f3b96ee413adce74548e4e7fc69a39427bc5c
tanmaymishra1551 <tanmaymishra1551@gmail.com>
2024-07-28 00:07:18 +0530

    edited variable name

--- backend/index.js
+++ backend/index.js
@@ -22,11 +22,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+// });
 
 
 connectDB()
